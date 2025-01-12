023b54ccb0ab7df2f2813a0cfb22b4ce732290aa
vishal dogra <dogravishal1598@gmail.com>
2025-01-12 15:18:09 +0530

    created task structure and added html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TasksComponent],
 })
 export class AppComponent {}
