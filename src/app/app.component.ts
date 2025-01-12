553cf74b7755865b19c3177f99b01d12c0937681
vishal dogra <dogravishal1598@gmail.com>
2025-01-12 13:08:53 +0530

    completed header structure and added css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [HeaderComponent],
 })
 export class AppComponent {}
